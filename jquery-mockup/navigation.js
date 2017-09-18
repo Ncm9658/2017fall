0a24edae7f928666f0faecd8e995964096ca2adf
Unknown <moyn1@hawkmail.newpaltz.edu>
2017-09-18 09:55:14 -0400

    Added jquery-mockup folder, Moved _footer.html, _header.html, _nav.html, index.html, index.css, navigation.js

--- jquery-mockup/navigation.js
+++ jquery-mockup/navigation.js
@@ -1,9 +1,9 @@
     $(function(){
-        $("#header-placeholder").load("_header.html");
-
-        $(".nav-link").removeClass("active");
-        $('.nav-link[href$="singup.html"]').addClass("active");
-
+        $("#header-placeholder").load("_header.html", function(){
+            $(".nav-link").removeClass("active");
+            $("#signup-link").addClass("active");
+        });
+        
         $(".btn-primary").click(function(){
             $(this).closest(".col-md-4").css({'max-width' : 'none'})
             .animate({'flex-basis' : '100%'})
