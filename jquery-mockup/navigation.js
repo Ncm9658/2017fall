854546b614d7be2ea0a366f9220201ca0d401ecd
Unknown <moyn1@hawkmail.newpaltz.edu>
2017-09-18 12:10:45 -0400

    Added package.json Updat jquery-mockup files

--- jquery-mockup/navigation.js
+++ jquery-mockup/navigation.js
@@ -1,11 +1,13 @@
-    $(function(){
-        $("#header-placeholder").load("_header.html", function(){
+$(function(){
+    $("#header-placeholder").load("_header.html", function(){
+        $("#nav-placeholder").load(navProperties.mainNav, function(){
             $(".nav-link").removeClass("active");
-            $("#signup-link").addClass("active");
-        });
-        
-        $(".btn-primary").click(function(){
-            $(this).closest(".col-md-4").css({'max-width' : 'none'})
-            .animate({'flex-basis' : '100%'})
+            $(`#${navProperties.currentSection}-link`).addClass("active");
         });
     });
+        
+    $(".btn-primary").click(function(){
+        $(this).closest(".col-md-4").css({'max-width' : 'none'})
+        .animate({'flex-basis' : '100%'})
+    });
+});
